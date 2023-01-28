4989de092cefd6c7006577a9e94f7321e3828ace
Josué Makuta <makutajosue@gmail.com>
2023-01-28 22:29:28 +0100

    Feat : Check if admin exists

--- controllers/admin/signup.js
+++ controllers/admin/signup.js
@@ -1,27 +1,28 @@
 import AdminModel from "../../models/admin/adminModel.js";
 
 const signUp = (req, res) => {
-  //   adminModel.findOne({ userEmail: req.body.userEmail }).then((user) => {
-  //     if (user) {
-  //       console.log("User exist");
-  //     } else {
-  console.log("User does not exist yet");
-  const admin = new AdminModel({
-    userName: req.body.userName,
-    userEmail: req.body.userEmail,
-    passWord: req.body.passWord,
+  AdminModel.findOne({ userEmail: req.body.userEmail }).then((user) => {
+    if (user) {
+      console.log("User exist");
+      res.end();
+    } else {
+      console.log("User does not exist yet");
+      const admin = new AdminModel({
+        userName: req.body.userName,
+        userEmail: req.body.userEmail,
+        passWord: req.body.passWord,
+      });
+      admin
+        .save()
+        .then(() => {
+          console.log("User saved");
+        })
+        .catch(() => {
+          console.log("An error occured when trying to save the user");
+        });
+      res.end();
+    }
   });
-  admin
-    .save()
-    .then(() => {
-      console.log("User saved");
-    })
-    .catch(() => {
-      console.log("An error occured when trying to save the user");
-    });
-  res.end();
 };
-//   });
-// };
 
 export default signUp;
