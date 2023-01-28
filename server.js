87ea27f227886093a527cea5d3c266c336c2cbdf
Josué Makuta <makutajosue@gmail.com>
2023-01-28 22:25:50 +0100

    Server : add headers

--- server.js
+++ server.js
@@ -24,6 +24,19 @@ app.use(express.json());
 // app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use("/admin", admin_router);
 app.use("/product", product_router);
