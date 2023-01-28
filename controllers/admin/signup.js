c9537e2ba5e67f99a58f6ab845804bab48632584
Josué Makuta <makutajosue@gmail.com>
2023-01-28 22:42:04 +0100

    Feat : Send json on success and on failure

--- controllers/admin/signup.js
+++ controllers/admin/signup.js
@@ -4,7 +4,7 @@ const signUp = (req, res) => {
   AdminModel.findOne({ userEmail: req.body.userEmail }).then((user) => {
     if (user) {
       console.log("User exist");
-      res.end();
+      res.status(403).json({ message: "User exists" });
     } else {
       console.log("User does not exist yet");
       const admin = new AdminModel({
@@ -16,11 +16,12 @@ const signUp = (req, res) => {
         .save()
         .then(() => {
           console.log("User saved");
+          res.status(200).json({ message: "User saved" });
         })
         .catch(() => {
           console.log("An error occured when trying to save the user");
+          res.status(500).json({ message: "Error server" });
         });
-      res.end();
     }
   });
 };
