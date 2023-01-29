9e18ec1ddbe665c79aa15eed7bd1a3c77d40b979
Josué Makuta <makutajosue@gmail.com>
2023-01-29 23:03:50 +0100

    Feat : Add product route

--- routes/products/products.js
+++ routes/products/products.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import addProduct from "../../controllers/products/add_product.js";
 import product_controller from "../../controllers/products/product_controller.js";
 
 const product_router = Router();
 
 product_router.get("/", product_controller);
+product_router.post("/add", addProduct);
 
 export default product_router;
