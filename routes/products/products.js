9117eba086c4943f1f48e0d0305d53c30cec8d7a
Josué Makuta <makutajosue@gmail.com>
2023-01-30 17:48:26 +0100

    Feat : Get products

--- routes/products/products.js
+++ routes/products/products.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import addProduct from "../../controllers/products/add_product.js";
+import getProducts from "../../controllers/products/get_products.js";
 import product_controller from "../../controllers/products/product_controller.js";
 
 const product_router = Router();
 
 product_router.get("/", product_controller);
 product_router.post("/add", addProduct);
+product_router.get("/get", getProducts);
 
 export default product_router;
