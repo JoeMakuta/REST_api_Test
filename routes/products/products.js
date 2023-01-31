53e8ad1c189ea013bf1bc0140da74eefd84b0c4b
Josué Makuta <makutajosue@gmail.com>
2023-01-31 15:32:18 +0100

    Feat : get InStock products

--- routes/products/products.js
+++ routes/products/products.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import addProduct from "../../controllers/products/add_product.js";
+import getInStockProducts from "../../controllers/products/get_inStock_products.js";
 import getOneProduct from "../../controllers/products/get_one_product.js";
 import getProducts from "../../controllers/products/get_products.js";
 import product_controller from "../../controllers/products/product_controller.js";
@@ -9,6 +10,7 @@ const product_router = Router();
 product_router.get("/", product_controller);
 product_router.post("/add", addProduct);
 product_router.get("/get", getProducts);
+product_router.get("/getInStock", getInStockProducts);
 product_router.get("/getOne/:id", getOneProduct);
 
 export default product_router;
