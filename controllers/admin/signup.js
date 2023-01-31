b929690f5ac189bb75d321a6bbe6513c5d5ab13a
Josué Makuta <makutajosue@gmail.com>
2023-01-31 23:55:00 +0100

    Feat : cryptpassword and save the hashed password

--- controllers/admin/signup.js
+++ controllers/admin/signup.js
@@ -1,23 +1,29 @@
 import AdminModel from "../../models/admin/adminModel.js";
 import signUpValidation from "../../validation/signUp_Validation.js";
+import bcrypt from "bcrypt";
 
-const signUp = (req, res) => {
+const signUp = async (req, res) => {
+  //Crypt the passoword
+
+  const salt = await bcrypt.genSalt(10);
+  const cryptedPassword = await bcrypt.hash(req.body.passWord, salt);
+
   //Validate input
 
   const validInput = signUpValidation(req.body);
   if (validInput.error) {
     res.status(400).send({ error: validInput.error.details[0].message });
   } else {
+    //check if the email is already registered
+
     AdminModel.findOne({ userEmail: req.body.userEmail }).then((user) => {
       if (user) {
-        console.log("User exist");
         res.status(403).json({ message: "User exists" });
       } else {
-        console.log("User does not exist yet");
         const admin = new AdminModel({
           userName: req.body.userName,
           userEmail: req.body.userEmail,
-          passWord: req.body.passWord,
+          passWord: cryptedPassword,
         });
         admin
           .save()
@@ -25,9 +31,9 @@ const signUp = (req, res) => {
             console.log("User saved");
             res.status(200).json({ message: "User saved" });
           })
-          .catch(() => {
+          .catch((err) => {
             console.log("An error occured when trying to save the user");
-            res.status(500).json({ message: "Error server" });
+            res.status(500).json({ message: "Error server" + err });
           });
       }
     });
