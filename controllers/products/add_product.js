5f3b6e41d8525bb701d29fb8d3a04d4566e4d52f
Josué Makuta <makutajosue@gmail.com>
2023-01-31 15:18:38 +0100

    Refac : Change save() to insertMany()

--- controllers/products/add_product.js
+++ controllers/products/add_product.js
@@ -1,30 +1,21 @@
 import ProductModel from "../../models/products/productModel.js";
 
 const addProduct = (req, res) => {
-  const { productName, productPrice, productDescription, inStock } = req.body;
-  
   ProductModel.findOne({ productName: req.body.productName }).then(
     (product) => {
       if (product) {
         console.log("Product exists");
-        res.status(403).json({ message: "Product exists" });
+        res.status(403).json({ message: "Product exists", data: product });
       } else {
         console.log("Product does not exist yet");
-        const admin = new ProductModel({
-          productName: productName,
-          productPrice: productPrice,
-          productDescription: productDescription,
-          inStock: inStock,
-        });
-        admin
-          .save()
+        ProductModel.insertMany(req.body)
           .then((product) => {
             console.log("Product saved");
             res.status(200).json({ message: "Product saved", data: product });
           })
           .catch((err) => {
             console.log("An error occured when trying to save the product");
-            res.status(500).json({ message: err });
+            res.status(500).json({ message: "Could not save the data", err: err });
           });
       }
     }
