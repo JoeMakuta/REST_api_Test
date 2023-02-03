8a179dee22fe4f83b05cd10e7e743e90cef7b618
Josué Makuta <makutajosue@gmail.com>
2023-02-03 10:13:40 +0100

    Feat : add exemple.env

--- controllers/products/get_one_product.js
+++ controllers/products/get_one_product.js
@@ -1,24 +1,26 @@
 import ProductModel from "../../models/products/productModel.js";
 
 const getOneProduct = (req, res) => {
-  ProductModel.findById(req.params.id)
-    .populate("postedBy", "_id, userName")
-    .then((data) => {
-      if (data) {
-        res.status(200).json({
-          message: data,
+  
+    ProductModel.findById(req.params.id)
+      .populate("postedBy", "_id, userName")
+      .then((data) => {
+        if (data) {
+          res.status(200).json({
+            message: data,
+          });
+        } else {
+          res.status(404).json({
+            err: "Product does not exist",
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+          err: err.message,
         });
-      } else {
-        res.status(404).json({
-          err: "Product does not exist",
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: err,
       });
-    });
+  
 };
 
 export default getOneProduct;
